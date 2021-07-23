a2adfe4a9a2acb830287589044b791d4e8163f04
so <ansso8430@gmail.com>
2021-07-23 17:13:45 +0900

    node

---
+++ exam/file/read_file_promise.js
@@ -0,0 +1,11 @@
+const fs = require('fs').promises; // 기존 제공 기능 > promise 방식으로 사용 가능 
+
+fs.readFile("./readme.txt")
+    .then((data) => {
+        //파일 읽기 성공
+        console.log(data);  //
+        console,log(data.toString()); //문자열로
+    })
+    .catch((err) => {
+        console.error(err);
+    });
