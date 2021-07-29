702724f7f7f88b3c3025567fbbbe25da6a6801b9
so <ansso8430@gmail.com>
2021-07-29 16:24:36 +0900

    tamplate

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res) => {
-
-    return res.render("main/index");
+    const data = {
+        addCss : ["main", "main2"],
+        addScript : ["main", "main2"],
+		pageTitle : "변경된 사이트 제목..",
+    }
+    return res.render("main/index", data);
 });
 
 module.exports = router;
