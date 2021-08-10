b8bc3315bd0bb83e0d3e7c563d93d4490b097fda
so <ansso8430@gmail.com>
2021-08-10 17:36:27 +0900

    part2

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -1,3 +1,14 @@
 <template>
-    <h1>제목...</h1>
+    <h1>{{ title }}</h1>
 </template>
+
+<script>
+export default {
+    props : {
+        title : {
+            type : String,
+            default : "기본 페이지 제목...",
+        }
+    }
+}
+</script>
