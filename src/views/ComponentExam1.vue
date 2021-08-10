511d5ed18edfc721fc6c553720eaa77bd6d830fc
so <ansso8430@gmail.com>
2021-08-10 17:45:49 +0900

    part2

--- src/views/ComponentExam1.vue
+++ src/views/ComponentExam1.vue
@@ -1,10 +1,10 @@
 <template>
-    <PageTitle title="메인페이지 제목1" />
-    <page-title title="메인페이지 제목2" />
+    <PageTitle title="메인페이지 제목1" v-bind:num="10" />
+    <page-title title="메인페이지 제목2" :num="20"/>
 </template>
 <script>
 import PageTitle from '../components/PageTitle.vue';
 export default {
-    components : {PageTitle},
+    components : { PageTitle },
 }
 </script>
