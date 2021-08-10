0ceab3f47b3909cc19f7dc8b7a57bd1248f7637f
so <ansso8430@gmail.com>
2021-08-10 17:14:12 +0900

    vue part1

---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+// import School2 from '../views/School2.vue'
+// import ListExam from '../views/ListExam.vue'
+// import IfExam from '../views/IfExam.vue'
+// import EventExam from '../views/EventExam.vue'
+// import EventExam2 from '../views/EventExam2.vue'
+// import ComputedExam from '../views/ComputedExam.vue'
+// import WatchExam from '../views/WatchExam.vue'
+import DataBindingVif from '../views/DataBindingVif.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/school',
+    name: 'school',
+    component: DataBindingVif,
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/DataBindingClass2.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container" :class="[activeClass, errorClass]">Class Binding</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            activeClass: 'active',
+            errorClass: 'text-red'
+        };
+    }
+}
+</script>
+<style scoped>
+.container {
+    width: 100%;
+    height: 200px;
+}
+
+.active {
+    background-color: yellow;
+    font-weight: bold;
+}
+
+.text-red {
+    color: red;
+}
+</style>
