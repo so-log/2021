10e7018393c43b0ce705cba7b6f7b1ea26c22044
so <ansso8430@gmail.com>
2021-08-11 18:51:41 +0900

    베스킨라빈스

--- js/script1.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $(".menu_button").click(function() {
-    $(".m_nav").addClass("on");
-  });
-  
-  $(".close_nav").click(function() {
-    $(".m_nav").removeClass("on");
-  });
-});
