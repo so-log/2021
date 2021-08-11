ee7145bd32e8860180443c3b4e151100d209dff5
so <ansso8430@gmail.com>
2021-08-11 21:17:52 +0900

    베스킨라빈스

--- js/baskin.js
+++ js/baskin.js
@@ -1,14 +1,14 @@
 const sideMenu = {
   open : function() {
-    if (!$(".m_nav, .close_nav").hasClass("on")) {
-      $(".m_nav, .close_nav").addClass("on");
+    if (!$(".m_nav, .close_nav, .br_main").hasClass("on")) {
+      $(".m_nav, .close_nav, .br_main").addClass("on");
     }
 
     $("#layer_dim").removeClass("dim");
   },
 
   close : function() {
-    $(".m_nav, .close_nav").removeClass("on");
+    $(".m_nav, .close_nav, .br_main").removeClass("on");
     if (!$("#layer_dim").hasClass("dim")) {
       $("#layer_dim").addClass("dim");
     }
