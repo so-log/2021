2518c998264429ece1fcb144380d686a3da1e98b
so <ansso8430@gmail.com>
2021-08-18 17:56:19 +0900

    데이터베이스 연동

---
+++ routes/board.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const board = require('../models/board');
+const router = express.Router();
+
+router.route('/write')
+    .get((req, res) => {
+        // 작성 양식
+        return res.render('board/form');
+    })
+    .post(async (req, res) => {
+        const num = await board.write(req.body);
+        console.log("num", num);
+    });
+
+module.exports = router;
