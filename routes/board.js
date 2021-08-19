9e4a73a43990b4f79dc651ba9d658dae7aaa6fe4
so <ansso8430@gmail.com>
2021-08-19 17:40:00 +0900

    board

--- routes/board.js
+++ routes/board.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-// 게시글 쓰기
-// 게시글 수정
+// 게시글 쓰기 -get 글쓰기 양식(form) -post 글쓰기
+// 게시글 수정 
 // 게시글 보기
 // 게시글 목록
+
+router.route("/")
+    .get((req, res) => {
+
+    })
+    .post((req, res) => {
+
+    });
 
 module.exports = router;
